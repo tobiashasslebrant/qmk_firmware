cb5fd7fb772eff6134fe40be25a0c45f2faa490d
Tobias Hasslebrant <tobias.hasslebrant@valtech.com>
2021-03-09 22:47:11 +0100

    libs

--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.12.15-3-g2aa38f"
-#define QMK_BUILDDATE "2021-03-09-09:21:52"
-#define CHIBIOS_VERSION "breaking_2021_q1"
-#define CHIBIOS_CONTRIB_VERSION "breaking_2021_q1"
+#define QMK_VERSION "0.8.132-2824-g045f4f-dirty"
+#define QMK_BUILDDATE "2021-03-09-22:45:11"
+#define CHIBIOS_VERSION "breaking_2020_q1-dirty"
+#define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1-dirty"
