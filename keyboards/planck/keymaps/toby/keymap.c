045f4f574ee6e4526ea08b4a8d2a4b60190a8cc3
Tobias Hasslebrant <tobias.hasslebrant@valtech.com>
2021-03-09 12:43:53 +0100

    added some features

--- keyboards/planck/keymaps/toby/keymap.c
+++ keyboards/planck/keymaps/toby/keymap.c
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_RCTL, KC_RALT, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_RCTL, KC_RALT, _______, _______, _______,
+    KC_LCTL, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Adjust (Lower + Raise)
@@ -102,10 +102,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______ ,
+    _______, RESET,   _______,    RGB_TOG, RGB_MODE_FORWARD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______ ,
     _______, _______, _______,  AU_ON,   AU_OFF, C(S(KC_PSCR)), C(A(KC_PSCR)), LGUI(KC_SPC),  _______, _______,  _______,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_LSFT, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+    KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 
